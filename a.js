b7a3848bfeb3d621d32c970a784ded0a47c0e7a3
Ayush Saklani <ayushsaklani04@gmail.com>
2023-11-23 02:02:30 +0530

    ground floor + 1st floor

--- a.js
+++ a.js
@@ -1,39 +1,49 @@
 // Create a Leaflet map
-const map = L.map(('map'),{center: [30.2734185,77.9997714],maxZoom:20,minZoom:18});
+const map = L.map(('map'),{center: [30.2734185,77.9997714],
+    doubleClickZoom:true
+    ,maxZoom:20,minZoom:18});
 
 // Add a base map (e.g., Mapbox)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
+    // attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// const geojsonLayer = L.geoJSON('og map.geojson').addTo(map);
-fetch('og map.geojson').then(response =>  response.json()).then(data => {
-                // Add GeoJSON layer to the map
-                L.geoJSON(data, {
-                    style: {
-                        color: 'cadetblue',
-                        weight: 1,
-                        opacity: 0.4,
-                    },
-                    onEachFeature: function (feature, layer) {
-                        if (feature.properties && feature.properties.label) {
-                            layer.bindPopup(feature.properties.label);
-                        }
-                    }
-                    ,
-                }).addTo(map);
+let level=-1;//front end se fetch karna level 
+if (level==-1) {
+    fetch('./mapgeoJSON/underground.geojson').then(response =>  response.json()).then(data => {
+        // Add GeoJSON layer to the map
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4,
+            },
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('Error loading GeoJSON file:', error));   
+}
+if(level == 1){//for ground floor
+    // const geojsonLayer = L.geoJSON('og map.geojson').addTo(map);
+    fetch('./mapgeoJSON/og map.geojson').then(response =>  response.json()).then(data => {
+        // Add GeoJSON layer to the map
+        L.geoJSON(data, {
+            style:{
+                color: 'cadetblue',
+                weight: 1,
+                opacity: 0.4,
+            },
+            // onEachFeature: function (feature, layer) {
+            //     if (feature.properties && feature.properties.label) {
+            //         layer.bindPopup(feature.properties.label);
+            //     }
+            // }, 
+        }).addTo(map);
+        // Fit the map to the bounds of the GeoJSON layer
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('Error loading GeoJSON file:', error));     
+}
 
-                // Fit the map to the bounds of the GeoJSON layer
-                map.fitBounds(L.geoJSON(data).getBounds());
-            })
-            .catch(error => console.error('Error loading GeoJSON file:', error));
-
-
-
-
-
-// const customRoute = L.polyline([
-//         [30.2731289, 77.9997726],
+    
+    
+    // const customRoute = L.polyline([
+        //         [30.2731289, 77.9997726],
 //         [30.2732541, 77.9998359],  // Add additional waypoints as needed
 //         [30.2733582, 77.9999662],  // Add additional waypoints as needed
 //         [30.2734329, 78.0001123],
@@ -58,23 +68,23 @@ fetch('og map.geojson').then(response =>  response.json()).then(data => {
 // const allLayers = L.layerGroup([customRoute, customRoute2, control]);
 //         map.fitBounds(allLayers.getBounds());
 
-const customPath = L.polyline([
-    [30.2731289, 77.9997726],    [30.2732541, 77.9998359],
-    [30.2733582, 77.9999662],    [30.2734329, 78.0001123],
-], { color: 'red' }).addTo(map);
-const customPath2 = L.polyline([
-    [30.2732541, 77.9998359],    
-    [30.2732565, 77.9996570],    
-], { color: 'blue' }).addTo(map);
-// Fit the map to the bounds of the custom path
+// const customPath = L.polyline([
+//     [30.2731289, 77.9997726],    [30.2732541, 77.9998359],
+//     [30.2733582, 77.9999662],    [30.2734329, 78.0001123],
+// ], { color: 'red' }).addTo(map);
+// const customPath2 = L.polyline([
+//     [30.2732541, 77.9998359],    
+//     [30.2732565, 77.9996570],    
+// ], { color: 'blue' }).addTo(map);
+// // Fit the map to the bounds of the custom path
 
 
-const control = L.Routing.control({
-    waypoints: [
-        L.latLng(30.2731289, 77.9997726),
-        L.latLng(30.2732565, 77.9996570),
-    ],
-}).addTo(map);
+// const control = L.Routing.control({
+//     waypoints: [
+//         L.latLng(30.2731289, 77.9997726),
+//         L.latLng(30.2732565, 77.9996570),
+//     ],
+// }).addTo(map);
 
-map.fitBounds(customPath2.getBounds());
-map.fitBounds(customPath.getBounds());
+// map.fitBounds(customPath2.getBounds());
+// map.fitBounds(customPath.getBounds());

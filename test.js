88bdbdf595bb6f34df30ff39e3cd15b6b9986ea1
“ayush <ayushsaklani04@gmail.com>
2024-01-26 22:40:59 +0530

    test in progress

--- test.js
+++ test.js
@@ -14,16 +14,24 @@ g.node(3).label='[30.2731101, 77.9997101]';
 g.node(4).label='[30.2731088, 77.9997088]';  
  
 var dijkstra = new jsgraphs.Dijkstra(g, 0);
- 
+let e;
 for(var v = 0; v < g.V; ++v){
     if(dijkstra.hasPathTo(v)){
+        let res = '';
         var path = dijkstra.pathTo(v);
         console.log('===================================');
         console.log('path from 0 to ' + v );
         for(var i = 0; i < path.length; ++i) {
-            var e = path[i];
-            console.log(e.from() + ' => ' + e.to() + ': ' + e.weight + ' ' + e.from().label);
+            e = path[i];
+            console.log(e.from() + ' => ' + e.to() + ': ' + e.weight);
+            res = res + g.node(e.from()).label + '@'
+            if(i==path.length -1){
+                res = res + g.node(e.to()).label;
+            }
         }
+        console.log(res);
+        let arr = res.split("@");
+        console.log(arr);
         console.log('distance: '  + dijkstra.distanceTo(v));
         console.log('===================================');
     }

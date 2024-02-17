63a8a9f9276868fe08018bc69e27fcff9ff90a16
“ayush <ayushsaklani04@gmail.com>
2024-02-17 20:44:17 +0530

    17-02-2024

--- a.js
+++ a.js
@@ -1,4 +1,10 @@
-const map = L.map(('map'),{center: [30.2734504,77.9997427],maxZoom:20,minZoom:18});
+const map = L.map(('map'),{
+    center: [30.2734504,77.9997427],
+    zoomAnimation:true,
+    maxZoom: 22,
+    maxNativeZoom: 18,
+    minZoom:17
+});
 const tileurl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const tile = L.tileLayer(tileurl).addTo(map);
 //base map from open streetmap added 
@@ -7,26 +13,121 @@ const tile = L.tileLayer(tileurl).addTo(map);
 // const points = [[30.2731289, 77.9997726],[30.2732541, 77.9998359],[30.2733582, 77.9999662],[30.2734329, 78.0001123]];
 // const    polytile = L.polyline(points).addTo(map);
 
-
-
-let level=-1;//front end se fetch karna level 
-
-fetch('./mapgeoJSON/floor'+level+'.geojson').then(response =>  response.json()).then(data => {
+fetch('./mapgeoJSON/floorG.geojson').then(response =>  response.json()).then(data => {
     L.geoJSON(data, {
         style:{color: 'cadetblue',weight: 1,opacity: 0.4},
     }).addTo(map);
     map.fitBounds(L.geoJSON(data).getBounds());
-}).catch(error => console.error('out of service.. ~_~  @_@', error));   
+}).catch(error => console.error('out of service.. ~_~  @_@', error));  
+
+document.getElementById('-1').addEventListener('click', () => {
+    map.eachLayer(function(layer) {
+        if (!!layer.toGeoJSON){map.removeLayer(layer);}
+    });
+    fetch('./mapgeoJSON/floor-1.geojson').then(response =>  response.json()).then(data => {
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4},
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+});
+document.getElementById('G').addEventListener('click', () => {
+    map.eachLayer(function(layer) {
+        if (!!layer.toGeoJSON){map.removeLayer(layer);}
+    });
+    fetch('./mapgeoJSON/floorG.geojson').then(response =>  response.json()).then(data => {
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4},
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+});
+document.getElementById('1').addEventListener('click', () => {
+    map.eachLayer(function(layer) {
+        if (!!layer.toGeoJSON){map.removeLayer(layer);}
+    });
+    fetch('./mapgeoJSON/floor1.geojson').then(response =>  response.json()).then(data => {
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4},
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+});
+document.getElementById('2').addEventListener('click', () => {
+    map.eachLayer(function(layer) {
+        if (!!layer.toGeoJSON){map.removeLayer(layer);}
+    });
+    fetch('./mapgeoJSON/floor2.geojson').then(response =>  response.json()).then(data => {
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4},
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+});
+document.getElementById('3').addEventListener('click', () => {
+    map.eachLayer(function(layer) {
+        if (!!layer.toGeoJSON){map.removeLayer(layer);}
+    });
+    fetch('./mapgeoJSON/floor3.geojson').then(response =>  response.json()).then(data => {
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4},
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+});
+document.getElementById('4').addEventListener('click', () => {
+    map.eachLayer(function(layer) {
+        if (!!layer.toGeoJSON){map.removeLayer(layer);}
+    });
+    fetch('./mapgeoJSON/floor4.geojson').then(response =>  response.json()).then(data => {
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4},
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+});
+document.getElementById('5').addEventListener('click', () => {
+    map.eachLayer(function(layer) {
+        if (!!layer.toGeoJSON){map.removeLayer(layer);}
+    });
+    fetch('./mapgeoJSON/floor5.geojson').then(response =>  response.json()).then(data => {
+        L.geoJSON(data, {
+            style:{color: 'cadetblue',weight: 1,opacity: 0.4},
+        }).addTo(map);
+        map.fitBounds(L.geoJSON(data).getBounds());
+    }).catch(error => console.error('out of service.. ~_~  @_@', error));   
+});
 
 
 
-let res = [
-    [30.2731289, 77.9997726],
-    [30.2732541, 77.9998359],
-    [30.2733582, 77.9999662],
-    [30.2734329, 78.0001123],
-];
-const customRoute = L.polyline(res,{ color: 'teal' }).addTo(map);
+
+
+
+// document.getElementById('getCoordinatesBtn').addEventListener('click', () => {
+//     fetch('/coordinates') // Send a GET request to the server
+//         .then(response => response.json()) // Parse the JSON response
+//         .then(data => {
+//             document.getElementById('coordinates').textContent = `Latitude: ${data.latitude}, Longitude: ${data.longitude}`;
+//         })
+//         .catch(error => console.error('Error:', error));
+// });
+
+
+// let res = [
+//     [30.2731289, 77.9997726],
+//     [30.2732541, 77.9998359],
+//     [30.2733582, 77.9999662],
+//     [30.2734329, 78.0001123],
+// ];
+// const customRoute = L.polyline(res,{ color: 'teal' }).addTo(map);
+
+
+
+
+
+
+
+
 
 // steelblue teal cadetblue
 

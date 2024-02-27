f0fafc2f9f378a8d7af51283bb6e9c2afa8214d8
“ayush <ayushsaklani04@gmail.com>
2024-02-27 19:31:56 +0530

    27-02-2024

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const map = L.map(('map'),{
     center: [30.2734504,77.9997427],
     zoomAnimation:true,
     maxZoom: 22,
-    minZoom:18
+    minZoom:19
 });
 const tileurl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const tile = L.tileLayer(tileurl,{
@@ -17,27 +17,13 @@ document.getElementById('go').addEventListener('click', () => {
     var source = document.getElementById("Start").value;
     var destination = document.getElementById("destination").value;
     
-    if(source>0 && source<1000){
-        floor=0;
-    }
-    else if(source>=1000 && source<2000){
-        floor=1;
-    }
-    else if(source>=2000 && source<3000){
-        floor=2;
-    }
-    else if(source>=3000 && source<4000){
-        floor=3;
-    }
-    else if(source>=4000 && source<5000){
-        floor=4;
-    }
-    else if(source>=5000 && source<6000){
-        floor=5;
-    }
-    else if(source>=6000 && source<7000){
-        floor=-1;
-    }
+    if(source>0 && source<1000)     floor=0;
+    else if(source>=1000 && source<2000)    floor=1;
+    else if(source>=2000 && source<3000)    floor=2;
+    else if(source>=3000 && source<4000)    floor=3;
+    else if(source>=4000 && source<5000)    floor=4;
+    else if(source>=5000 && source<6000)    floor=5;
+    else if(source>=6000 && source<7000)    floor=-1;
     console.log(source);
     console.log(destination);
     fetch('http://127.0.0.1:3000/getCoordinates', {
@@ -54,7 +40,6 @@ document.getElementById('go').addEventListener('click', () => {
         map.eachLayer(function(layer){if (layer.toGeoJSON){map.removeLayer(layer);}});
         fetch(`./mapgeoJSON/floor${floor}.geojson`).then(response =>  response.json()).then(data => {
             L.geoJSON(data,{style:{color: 'cadetblue',weight: 1,opacity: 1}}).addTo(map);
-            // map.fitBounds(L.geoJSON(data).getBounds());
         }).catch(error => console.error('out of service.. ~_~  @_@', error));
         if(floor==-1){
             temp_point = points[6];
@@ -73,12 +58,15 @@ document.getElementById('go').addEventListener('click', () => {
     .catch(error => console.error('Error hai bhaisaab:', error));
 });
 
+
+// intitialize  the map with default view and zoom level
 fetch('./mapgeoJSON/floor0.geojson').then(response =>  response.json()).then(data => {
     L.geoJSON(data, {
         style:{color: 'cadetblue',weight: 1,opacity: 1},
     }).addTo(map);
     map.fitBounds(L.geoJSON(data).getBounds());
 }).catch(error => console.error('out of service.. ~_~  @_@', error));  
+
 
 
 
@@ -101,7 +89,6 @@ document.getElementById('-1').addEventListener('click', () => {
       }).addTo(map);
 });
 
-
 document.getElementById('G').addEventListener('click', () => {
     map.eachLayer(function(layer) {
         if (!!layer.toGeoJSON){map.removeLayer(layer);}
@@ -121,7 +108,6 @@ document.getElementById('G').addEventListener('click', () => {
       }).addTo(map);
 });
 
-
 document.getElementById('1').addEventListener('click', () => {
     map.eachLayer(function(layer) {
         if (!!layer.toGeoJSON){map.removeLayer(layer);}
@@ -139,6 +125,7 @@ document.getElementById('1').addEventListener('click', () => {
         "pulseColor": "#000000",
       }).addTo(map);
 });
+
 document.getElementById('2').addEventListener('click', () => {
     map.eachLayer(function(layer) {
         if (!!layer.toGeoJSON){map.removeLayer(layer);}
@@ -156,6 +143,7 @@ document.getElementById('2').addEventListener('click', () => {
         "pulseColor": "#000000",
       }).addTo(map);
 });
+
 document.getElementById('3').addEventListener('click', () => {
     map.eachLayer(function(layer) {
         if (!!layer.toGeoJSON){map.removeLayer(layer);}
@@ -173,6 +161,7 @@ document.getElementById('3').addEventListener('click', () => {
         "pulseColor": "#000000",
       }).addTo(map);
 });
+
 document.getElementById('4').addEventListener('click', () => {
     map.eachLayer(function(layer) {
         if (!!layer.toGeoJSON){map.removeLayer(layer);}
@@ -190,6 +179,7 @@ document.getElementById('4').addEventListener('click', () => {
         "pulseColor": "#000000",
       }).addTo(map);
 });
+
 document.getElementById('5').addEventListener('click', () => {
     map.eachLayer(function(layer) {
         if (!!layer.toGeoJSON){map.removeLayer(layer);}
@@ -207,6 +197,7 @@ document.getElementById('5').addEventListener('click', () => {
         "pulseColor": "#000000",
       }).addTo(map);
 });
+
 document.getElementById('R').addEventListener('click', () => {
     map.eachLayer(function(layer) {
         if (!!layer.toGeoJSON){map.removeLayer(layer);}

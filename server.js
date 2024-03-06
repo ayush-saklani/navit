975cfd4c1cc6ed4f341b0ded3f78b48aebb64e01
“ayush <ayushsaklani04@gmail.com>
2024-03-06 15:13:31 +0530

    06-03-2024

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 
 // this funciton takes maps from map.mjs and returns the polyline coordinates
-let dijfunc = (src,des) =>{
+let dijfunc__version_1 = (src,des) =>{ 
     var dijkstra = new jsgraphs.Dijkstra(g, src);
     let e;
     if(dijkstra.hasPathTo(des)){
@@ -27,6 +27,54 @@ let dijfunc = (src,des) =>{
                 temparr.push(Number(temp[0]));
                 temparr.push(Number(temp[1]));
                 res[temp[2]].push(temparr)
+            }
+        }
+        return res;
+    }
+    else{
+        console.log('error hai bhaisaab service down hai shayad');
+    }
+}
+let dijfunc2 = (src,des) =>{
+    var dijkstra = new jsgraphs.Dijkstra(g, src);
+    let e;
+    if(dijkstra.hasPathTo(des)){
+        let res = [[],[],[],[],[],[],[]];
+        var path = dijkstra.pathTo(des);
+        let curr_floor =0;
+        let temp_con=[];
+        for(var i = 0; i < path.length; ++i){
+            e = path[i];
+            let temp = (g.node(e.from()).label).split(','); 
+            if(i==0){
+                curr_floor = temp[2];
+            }
+            let temparr = []
+            if(i!=path.length -1){
+                temparr.push(Number(temp[0]));
+                temparr.push(Number(temp[1]));
+                if(temp[2]==curr_floor){
+                    temp_con.push(temparr)
+                }
+                else{
+                    res[curr_floor].push(temp_con);
+                    temp_con=[]
+                    temp_con.push(temparr)
+                    curr_floor=temp[2];
+                }
+            }
+            else if(i==path.length -1){
+                let temp = (g.node(e.from()).label).split(','); 
+                let temparr = []
+                temparr.push(Number(temp[0]));
+                temparr.push(Number(temp[1]));
+                temp_con.push(temparr)
+                temp = (g.node(e.to()).label).split(','); 
+                temparr = []
+                temparr.push(Number(temp[0]));
+                temparr.push(Number(temp[1]));
+                temp_con.push(temparr)
+                res[curr_floor].push(temp_con);
             }
         }
         return res;
@@ -83,9 +131,9 @@ app.post('/getCoordinates', (req, res) => {
         console.log(src);
         console.log(des);
         let temp_des = nearest_amenity(src, des);
-        coordinates = dijfunc(src, temp_des);
+        coordinates = dijfunc2(src, temp_des);
     }else{
-        coordinates = dijfunc(src, des);
+        coordinates = dijfunc2(src, des);
     }
 
     // Check if coordinates were found
--- tester.js
+++ tester.js
@@ -1,11 +1,5 @@
 import jsgraphs from 'js-graph-algorithms';
 import {g} from './newmap.js'
-import cors from 'cors'; // Import the cors middleware
-import express from 'express'
-const app = express()
-const port = 3000
-app.use(cors());
-
 
 let dijfunc2 = (src,des) =>{
     var dijkstra = new jsgraphs.Dijkstra(g, src);
@@ -13,25 +7,35 @@ let dijfunc2 = (src,des) =>{
     if(dijkstra.hasPathTo(des)){
         let res = [[],[],[],[],[],[],[]];
         var path = dijkstra.pathTo(des);
-        let floor = (g.node(path[0].from()).label).split(',')[2];
-        let temparr2 =[];
-        let tempo;
-        for(var i = 0; i < path.length; ++i) {
+        let curr_floor =0;
+        let temp_con=[];
+        for(var i = 0; i < path.length; ++i){
             e = path[i];
             let temp = (g.node(e.from()).label).split(','); 
-            let temparr = []
-            temparr.push(Number(temp[0]));
-            temparr.push(Number(temp[1]));
-            res[temp[2]].push(temparr)
-            if(floor-temp[2]!= 0){
-                
+            if(i==0){
+                curr_floor = temp[2];
             }
-            if(i==path.length -1){
+            let temparr = []
+            if(i!=path.length -1){
+                temparr.push(Number(temp[0]));
+                temparr.push(Number(temp[1]));
+                if(temp[2]==curr_floor){
+                    temp_con.push(temparr)
+                }
+                else{
+                    res[curr_floor].push(temp_con);
+                    temp_con=[]
+                    temp_con.push(temparr)
+                    curr_floor=temp[2];
+                }
+            }
+            else if(i==path.length -1){
                 let temp = (g.node(e.to()).label).split(','); 
                 let temparr = []
                 temparr.push(Number(temp[0]));
                 temparr.push(Number(temp[1]));
-                res[temp[2]].push(temparr)
+                temp_con.push(temparr)
+                res[curr_floor].push(temp_con);
             }
         }
         return res;
@@ -40,41 +44,5 @@ let dijfunc2 = (src,des) =>{
         console.log('error hai bhaisaab service down hai shayad');
     }
 }
-
-let res = dijfunc2(3084, 3085);
+let res = dijfunc2(1001, 1127);
 console.log(res);
-
-
-
-
-
-// // Middleware to parse JSON bodies
-// app.use(express.json());
-
-// // Route to handle POST requests from the HTML page
-// app.post('/getCoordinates', (req, res) => {
-//     const { src, des } = req.body;
-    
-//     // Execute the dijfunc function with the provided source and destination
-//     let coordinates;
-//     if (des==999 || des==998 ) {
-//         console.log(src);
-//         console.log(des);
-//         let temp_des = nearest_amenity(src, des);
-//         coordinates = dijfunc(src, temp_des);
-//     }else{
-//         coordinates = dijfunc(src, des);
-//     }
-
-//     // Check if coordinates were found
-//     if (coordinates) {
-//         res.json(coordinates);
-//     } else {
-//         res.status(404).json({ error: 'No path found.' });
-//     }
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//     console.log(`Server is listening at http://localhost:${port}`);
-// });

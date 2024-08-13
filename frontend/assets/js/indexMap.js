2177cdc37e03df3a7742628bcae401bbe749cf64
“ayush <ayushsaklani04@gmail.com>
2024-08-13 21:04:34 +0530

    ui improv ++

--- frontend/assets/js/indexMap.js
+++ frontend/assets/js/indexMap.js
@@ -13,21 +13,36 @@ map.on('zoomend', function() {
         document.querySelectorAll('.text-icon-size').forEach(element => {
             element.style.fontSize = '0px';
         });
+        document.querySelectorAll('.text-icon-hide').forEach(element => {
+            element.style.visibility = 'hidden';
+        });
     }else if(currzoom >= 18 && currzoom < 20){
         document.querySelectorAll('.text-icon-size').forEach(element => {
             element.style.fontSize = 'smaller';
+        });
+        document.querySelectorAll('.text-icon-hide').forEach(element => {
+            element.style.visibility = 'hidden';
         });
     }else if(currzoom >= 20 && currzoom < 21){
         document.querySelectorAll('.text-icon-size').forEach(element => {
             element.style.fontSize = 'small';
         });
+        document.querySelectorAll('.text-icon-hide').forEach(element => {
+            element.style.visibility = 'hidden';
+        });
     }else if(currzoom >= 21 && currzoom < 22){
         document.querySelectorAll('.text-icon-size').forEach(element => {
             element.style.fontSize = 'large';
         });
+        document.querySelectorAll('.text-icon-hide').forEach(element => {
+            element.style.visibility = 'visible';
+        });
     }else if(currzoom >= 22){
         document.querySelectorAll('.text-icon-size').forEach(element => {
             element.style.fontSize = 'x-large';
+        });
+        document.querySelectorAll('.text-icon-hide').forEach(element => {
+            element.style.display = 'block';
         });
     }
 });
@@ -218,8 +233,8 @@ const renderRoomStatusAndDetail = (floordata) => {
     const weekdays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
     let day_slot = weekdays[today.getDay()];
     let hours = today.getHours();
-    hours = 10;  // for testing
-    if (hours == 0) hours = 12;          // 12 am is 0 in js
+    // hours = 10;                          // for testing
+    if (hours == 0) hours = 12;             // 12 am is 0 in js
     let houre = hours + 1;
     hours = (hours > 12) ? String(hours - 12).padStart(2, "0") : String(hours).padStart(2, "0");
     houre = (houre > 12) ? String(houre - 12).padStart(2, "0") : String(houre).padStart(2, "0");
@@ -233,7 +248,8 @@ const renderRoomStatusAndDetail = (floordata) => {
     if (!timeslots.includes(time_slot)) {
         time_slot = "08-09";
     }
-    if((hours >= 18 && hours <= 23) || (hours >= 0 && hours <= 7)){
+    // if((hours >= 18 && hours <= 23) || (hours >= 0 && hours <= 7)){     // for testing only
+    if((today.getHours() >= 18 && today.getHours() <= 23) || (today.getHours() >= 0 && today.getHours() <= 7)){
         for (room in room_status_data) {
             floordata.features.forEach(feature => {
                 if (feature.properties && feature.properties.room_id && feature.properties.room_id == room_status_data[room].roomid) {
@@ -256,6 +272,36 @@ const renderRoomStatusAndDetail = (floordata) => {
                 }
             });
         }
+        floordata.features.forEach(feature => {             // for aminities like washroom etc
+            const aminities = [
+                "1051", "1029", "1099", "1083",
+                "2051", "2029", "2099", "2083",
+                "3051", "3029", "3099", "3083",
+                "4051", "4029", "4099", "4083",
+                "5051", "5029", "5099", "5083",
+                "6051", "6029", "6099", "6083"
+            ]
+            if (feature.properties && feature.properties.room_id && aminities.includes(feature.properties.room_id)) {
+                let cc = getSpecificRoomCoordinates(floordata, feature.properties.room_id);
+                let polygon = L.polygon(cc, { 
+                    color : "var(--Hard-Background)",
+                    opacity: 0.1,
+                    fillColor: "DarkCyan",
+                    fillOpacity: 0.5,
+    
+                }).addTo(map);
+                let center = polygon.getBounds().getCenter();
+                let textIcon = L.divIcon({
+                    className: 'text-icon-white text-icon-size',
+                    html: "WC",
+                    iconSize: [0, 0],
+                    iconAnchor: [0, 0]
+                });
+                L.marker(center, { icon: textIcon }).addTo(map);
+                // Add a marker with the text icon at the center of the polygon 
+            }
+        });
+        document.getElementById("currtime").innerHTML = `<i class="bi bi-moon-stars-fill pe-2"></i> Campus closed`;
         LoaderManager(0);
         return ;
     }
@@ -277,6 +323,14 @@ const renderRoomStatusAndDetail = (floordata) => {
                     let temproomdata = room_status_data[room].schedule[day_slot][time_slot];
                     let textIcon = L.divIcon({
                         className: 'text-icon text-icon-size',
+                        html: room_status_data[room].name,
+                        iconSize: [0, 0],
+                        iconAnchor: [0, 0]
+                    });
+                    L.marker(center, { icon: textIcon }).addTo(map);
+                    temproomdata = room_status_data[room].schedule[day_slot][time_slot];
+                    textIcon = L.divIcon({
+                        className: 'text-icon text-icon-size text-icon-hide',
                         html: room_status_data[room].name +" "+ temproomdata.course +" Section:" +temproomdata.section + " " + temproomdata.subjectcode + " " ,
                         iconSize: [0, 0],
                         iconAnchor: [0, 0]

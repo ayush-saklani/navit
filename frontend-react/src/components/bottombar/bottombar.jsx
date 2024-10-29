09c12fc21b797759cd805ceaa712d6a801c4f60f
“ayush <ayushsaklani04@gmail.com>
2024-10-29 18:44:20 +0530

    react refactoring and updates

--- frontend-react/src/components/bottombar/bottombar.jsx
+++ frontend-react/src/components/bottombar/bottombar.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
-import Option1 from './option1'
-import Option2 from './option2'
 import './bottombar.css'
+import roomData from './room.json'
 const bottombar = () => {
-  return (
-    <div className="bottom-bar ">
+    return (
+        <div className="bottom-bar ">
             <button className="countboxi" id="foldup">
                 <i id="foldup-icon" className="bi bi-caret-down-fill h3"></i>
             </button>
             <div className="container row justify-content-center mb-2">
                 <div className="form-floating col-lg-5 col-md-5 col-sm-12 pb-1 text">
-                    <select className="form-select"  id="Start">
-                        <Option1 />
+                    <select className="form-select" id="Start">
+                        {roomData.rooms.map((room, index) => (
+                            <optgroup label={room.floor_label} key={index}>
+                                {room.room_data.map((subroom, subindex) => (
+                                    <option key={subindex} value={subroom.roomid}>{subroom.room_name}</option>
+                                ))}
+                            </optgroup>
+                        ))}
                     </select>
                     <label className="ms-2 text coloring"><b>Start</b></label>
                 </div>
                 <div className="form-floating col-lg-5 col-md-4 col-sm-12 pb-1 text">
                     <select className="form-select" id="destination">
-                        <Option2 />
-                        <Option1 />
+                        {roomData.amenities.map((room, index) => (
+                            <optgroup label={room.floor_label} key={index}>
+                                {room.room_data.map((subroom, subindex) => (
+                                    <option key={subindex} value={subroom.roomid}>{subroom.room_name}</option>
+                                ))}
+                            </optgroup>
+                        ))}
+                        {roomData.rooms.map((room, index) => (
+                            <optgroup label={room.floor_label} key={index}>
+                                {room.room_data.map((subroom, subindex) => (
+                                    <option key={subindex} value={subroom.roomid}>{subroom.room_name}</option>
+                                ))}
+                            </optgroup>
+                        ))}
                     </select>
                     <label className="ms-2 text coloring"><b>Destination</b></label>
                 </div>
@@ -27,7 +44,7 @@ const bottombar = () => {
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default bottombar

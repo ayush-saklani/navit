41638e26cf055c5e4dddf60a32cb8cfbfddc3a24
ayush saklani <ayushsaklani04@gmail.com>
2024-12-20 00:41:43 +0530

    updates

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -10,32 +10,40 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { FaLinkedinIn } from 'react-icons/fa6'
 
 function App() {
+    const [activeFloor, setActiveFloor] = useState(0)
+    const floors = [-1, 0, 1, 2, 3, 4, 5]
+    const [timevar, setTimevar] = useState(null)
     return (
         <div>
+            <div className="position-fixed bottom-0 fw-bold left-0 text-lg text-brand-primary-dark px-2 fw-bold z-[1]">Hits</div>
             <nav className="flex align-items-center p-2 position-fixed z-[1]">
-                <img className="h-[88px]" src={navitlogo} height="80px" />
-                <h1 className="text text-brand-primary-dark mx-2 text-5xl"><b>Navit</b></h1>
+                <img className="h-[80px]" src={navitlogo} height="70px" />
+                <h1 className="text text-brand-primary-dark mx-2 text-4xl"><b>Navit</b></h1>
             </nav>
-            <footer className="position-fixed top-0 right-0 m-1 flex gap-1 z-[1]" >
-                <a href="https://github.com/ayush-saklani" target="_blank">
-                    <FaGithub size={44} className='bg-brand-primary rounded-xl p-2 hover:bg-brand-primary-light text-foreground-1' />
-                </a>
-                <a href="https://www.linkedin.com/in/ayush-saklani/" target="_blank">
-                    <FaLinkedinIn size={44} className='bg-brand-primary rounded-xl p-2 hover:bg-brand-primary-light text-foreground-1' />
-                </a>
-            </footer >
-            <floorbutton>
-                <div className="floorbutton btn-group-vertical position-fixed bottom-0 start-0 top-0 m-3 " role="group">
-                    <button className="circular_button" id="-1"><b>-1</b></button>
-                    <button className="circular_button active" id="0"><b>G</b></button>
-                    <button className="circular_button" id="1"><b>1</b></button>
-                    <button className="circular_button" id="2"><b>2</b></button>
-                    <button className="circular_button" id="3"><b>3</b></button>
-                    <button className="circular_button" id="4"><b>4</b></button>
-                    <button className="circular_button" id="5"><b>5</b></button>
-                </div>
+
+            <MapContainer
+                center={[30.2734504, 77.9997427]}
+                zoom={18}
+                maxZoom={22}
+                minZoom={16}
+                zoomControl={false}
+                scrollWheelZoom={true}
+                style={{ height: "100vh", width: "100%" }}
+                id='map'
+            >
+                <TileLayer
+                    attribution='<p><a href="https://github.com/ayush-saklani"><img src="https://flagcdn.com/in.svg" width="15" alt="India"><b> Made by Ayush Saklani</b></a> <br>Co-powered by <a href="https://github.com/ayush-saklani/classsync"><b>Classsync</b></a></p>'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+            </MapContainer>
+
+            <floorbutton className="floorbutton mx-3 flex gap-1 justify-content-center align-items-center z-[1] flex-column position-fixed  left-0 top-[14%]" >
+                {floors.map((floor, index) => (
+                    <button key={index} className={`circular_button aspect-square ${activeFloor === floor ? 'active' : ''}`} id={floor} onClick={() => setActiveFloor(floor)}><b>{floor === -1 ? 'B' : floor === 0 ? 'G' : floor}</b></button>
+                ))}
             </floorbutton>
-            <div className="bottom-bar ">
+
+            <bottombar className="bottom-bar ">
                 <button className="countboxi" id="foldup">
                     <i id="foldup-icon" className="bi bi-caret-down-fill h3"></i>
                 </button>
@@ -75,23 +83,19 @@ function App() {
                         <button type="button" className="btn btn-lg btn-light coloring rounded-pill col-12 px-2" id="go"><b className="h4 fw-bold">Go</b></button>
                     </div>
                 </div>
-            </div>
+            </bottombar>
 
-            <MapContainer
-                center={[30.2734504, 77.9997427]}
-                zoom={18}
-                maxZoom={22}
-                minZoom={16}
-                zoomControl={false}
-                scrollWheelZoom={true}
-                style={{ height: "100vh", width: "100%" }}
-                id='map'
-            >
-                <TileLayer
-                    attribution='<p><a href="https://github.com/ayush-saklani"><img src="https://flagcdn.com/in.svg" width="15" alt="India"><b> Made by Ayush Saklani</b></a> <br>Co-powered by <a href="https://github.com/ayush-saklani/classsync"><b>Classsync</b></a></p>'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-            </MapContainer>
+            <footer className="fixed top-0 right-0 m-1 z-[1] flex flex-col gap-1 align-items-center justify-content-center">
+                <div className="text-xl px-1 text-brand-primary-dark fw-bold">
+                    {timevar !== null ? timevar : <div className="spinner-border text-2xl text-brand-primary-dark p-3"></div>}
+                </div>
+                <a href="https://github.com/ayush-saklani" target="_blank">
+                    <FaGithub size={44} className='bg-brand-primary rounded-xl p-2 hover:bg-brand-primary-light text-foreground-1' />
+                </a>
+                <a href="https://www.linkedin.com/in/ayush-saklani/" target="_blank">
+                    <FaLinkedinIn size={44} className='bg-brand-primary rounded-xl p-2 hover:bg-brand-primary-light text-foreground-1' />
+                </a>
+            </footer >
         </div>
     )
 }

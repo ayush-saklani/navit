acf6cddc706387cceb3806d55f4ffc20aba0e922
ayush saklani <ayushsaklani04@gmail.com>
2025-06-04 18:46:06 +0530

    cors

--- backend/server2.js
+++ backend/server2.js
@@ -38,6 +38,7 @@ app.use(
         credentials: true,
     })
 );
+app.options('*', cors());
 app.use(express.json());                    // Middleware to parse JSON bodies        
 
 const g = new jsgraphs.WeightedGraph(7000); // Create a graph with 7000 nodes
